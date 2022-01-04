92baf465a19ba0b7ea2ad6bbea2df8ef3d9c958f
BERGES Dimitri <dixmis@hotmail.fr>
2022-01-04 10:57:17 +0100

    Initial commit with Firebase included

---
+++ app/src/main/java/fr/lomis/iut/m4101c2021_2022/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.lomis.iut.m4101c2021_2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
